1b3ba874cbbea81a48c4eeea10073444f482aa2c
laksh <laksh@LakshmiPC>
2022-09-23 16:47:16 +0530

    push

---
+++ trialproject/src/test/java/generic/Auto_const.java
@@ -0,0 +1,13 @@
+package generic;
+
+public interface Auto_const
+{
+
+	String gecko_key = "webdriver.gecko.driver";
+	String gecko_value = "C://Users//laksh//Downloads//geckodriver-v0.31.0-win32//geckodriver.exe";
+	String chrome_key = "webdriver.chrome.driver";
+	String chrome_value = "C://Users//laksh//Downloads//chromedriver_win32//chromedriver.exe";
+	String edge_key = "webdriver.edge.driver";
+	String edge_value = "C://Users//laksh//Downloads//edgedriver_win64//edgedriver.exe";
+	String excel_path = "./excel/Exelsheet.xlsx";
+}
